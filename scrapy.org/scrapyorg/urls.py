ffc8eddce67d165e3fa5b1f5b288c7a232a47b9d
pablo <devnull@localhost>
2009-08-14 11:08:02 -0300

    no more ie6 hacks

--- scrapy.org/scrapyorg/urls.py
+++ scrapy.org/scrapyorg/urls.py
@@ -6,8 +6,6 @@ urlpatterns = patterns('django.views.generic.simple',
     url(r"^download/", 'direct_to_template', {'template': 'pages/download.html'}),
     url(r"^doc/", 'direct_to_template', {'template': 'pages/doc.html'}),
     url(r"^community/", 'direct_to_template', {'template': 'pages/community.html'}),
-    # for ie6 png hack
-    url(r"^blank.gif$", 'redirect_to', {'url': '%s/style/blank.gif' % settings.MEDIA_URL})
 )
 
 if settings.DEBUG: # devel
