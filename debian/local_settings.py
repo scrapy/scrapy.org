3d32f976ca81d0cf76c2898c67357781d533ceaf
Pablo Hoffman <pablo@pablohoffman.com>
2012-03-20 09:22:39 -0300

    fixed typo: scrapyorh -> scrapyorg

--- debian/local_settings.py
+++ debian/local_settings.py
@@ -1,5 +1,5 @@
 # This file is deployed by APT and not recommended to be modified manually.
-# Instead, add a /etc/scrapyorh/server_settings.py file for your custom
+# Instead, add a /etc/scrapyorg/server_settings.py file for your custom
 # settings.
 
 import socket
@@ -7,17 +7,17 @@ import socket
 DEBUG = False
 
 MAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_SUBJECT_PREFIX = '[scrapyorh] '
-SERVER_EMAIL = 'scrapyorh@%s' % socket.getfqdn()
+EMAIL_SUBJECT_PREFIX = '[scrapyorg] '
+SERVER_EMAIL = 'scrapyorg@%s' % socket.getfqdn()
 ADMINS = [('Errors', 'errors@scrapinghub.com')]
 
 # scrapyorg doesn't require a database (yet)
 #DATABASE_ENGINE = 'sqlite3'
-#DATABASE_NAME = '/var/lib/scrapyorh/scrapyorh.db'
+#DATABASE_NAME = '/var/lib/scrapyorg/scrapyorg.db'
 #DATABASE_USER = ''
 #DATABASE_PASSWORD = ''
 
-TEMPLATE_DIRS = ['/usr/share/scrapyorh/templates']
+TEMPLATE_DIRS = ['/usr/share/scrapyorg/templates']
 MEDIA_ROOT = '/usr/share/scrapyorg/static'
 
 try:
