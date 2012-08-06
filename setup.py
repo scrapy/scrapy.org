e786aeb800cefacb78eb2ed60a031d89bf9c21c0
Daniel Grana <dangra@gmail.com>
2012-08-06 10:32:39 -0300

    disable tests

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'scrapyorg',
-    version      = '1.0',
-    packages     = find_packages(),
+    name = 'scrapyorg',
+    version = '1.0',
+    packages = find_packages(),
+    test_suite = '',
 )
