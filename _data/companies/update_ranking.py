9356d60d795b3e941738040929a510ef04cf2539
Paul Tremberth <paul.tremberth@gmail.com>
2017-07-21 16:50:58 +0200

    Add data files for company contributions and ranking

---
+++ _data/companies/update_ranking.py
@@ -0,0 +1,20 @@
+from csv import DictReader, DictWriter
+from collections import defaultdict
+from operator import itemgetter
+
+with open('points.csv') as f:
+    reader = DictReader(f)
+    catpoints = {r['category']: int(r['points']) for r in reader}
+
+points = defaultdict(int)
+with open('contributions.csv') as f:
+    reader = DictReader(f)
+    for r in reader:
+        points[r['company']] += catpoints[r['category']]
+
+ranking = dict(sorted(points.items(), key=itemgetter(1), reverse=True))
+
+with open('ranking.csv', 'w') as f:
+    writer = DictWriter(f, ['company', 'score'])
+    writer.writeheader()
+    writer.writerows({'company': c, 'score': s} for (c, s) in ranking.items())
